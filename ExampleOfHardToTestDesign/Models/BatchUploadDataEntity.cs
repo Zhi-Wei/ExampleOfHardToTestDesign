4c913b96314632a5aba0962507341842628d04ab
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-05-30 09:44:27 +0800

    新建 BatchUploadDataEntity 批次上傳資料實體類別。

---
+++ ExampleOfHardToTestDesign/Models/BatchUploadDataEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExampleOfHardToTestDesign.Models
+{
+    /// <summary>
+    /// 批次上傳資料實體類別。
+    /// </summary>
+    public class BatchUploadDataEntity
+    {
+        /// <summary>
+        /// 預訂時間。
+        /// </summary>
+        public DateTime BookingTime { get; set; }
+    }
+}
