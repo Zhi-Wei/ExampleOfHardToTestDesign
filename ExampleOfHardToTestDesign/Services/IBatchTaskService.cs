792373a665d7ac8e361d74c4ee92ef1ba3a0f8e7
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-06-01 14:15:37 +0800

    1. 重新命名 BatchUploadDataEntity 類別為 BatchTaskDataEntity。 2. 重新命名 IBatchUploadService 介面為 IBatchTaskService。 3. 安裝 Newtonsoft.Json 11.0.2 套件。 4. 新建 BatchTaskService 類別並實作 CreateBatchTasks 方法。

---
+++ ExampleOfHardToTestDesign/Services/IBatchTaskService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ExampleOfHardToTestDesign.Models;
+
+namespace ExampleOfHardToTestDesign.Services
+{
+    /// <summary>
+    /// 定義批次任務服務的方法。
+    /// </summary>
+    public interface IBatchTaskService
+    {
+        /// <summary>
+        /// 新增批次任務，若全部新增成功則回傳 <c>true</c> 否則為 <c>false</c>。
+        /// </summary>
+        /// <param name="batchUploadData">批次任務資料集。</param>
+        /// <returns>若全部新增成功則回傳 <c>true</c> 否則為 <c>false</c>。</returns>
+        bool CreateBatchTasks(IEnumerable<BatchTaskDataEntity> batchUploadData);
+    }
+}
