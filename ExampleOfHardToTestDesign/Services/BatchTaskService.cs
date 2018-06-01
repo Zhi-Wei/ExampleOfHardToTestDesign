97cb773069a6c6a55e57b08bb20c0adc4774be0c
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-06-01 14:48:28 +0800

    最佳化 BatchTaskService 類別 using 的命名空間。

--- ExampleOfHardToTestDesign/Services/BatchTaskService.cs
+++ ExampleOfHardToTestDesign/Services/BatchTaskService.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ExampleOfHardToTestDesign.Models;
+using ExampleOfHardToTestDesign.Utilities;
 using Newtonsoft.Json;
 
 namespace ExampleOfHardToTestDesign.Services
