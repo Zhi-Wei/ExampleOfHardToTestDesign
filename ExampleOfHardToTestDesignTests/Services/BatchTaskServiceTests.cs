fba261b8db18473f6f66e02cf9084de4c0ea1461
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-06-04 17:52:16 +0800

    新建 BatchTaskServiceTests 批次任務服務的測試類別。

---
+++ ExampleOfHardToTestDesignTests/Services/BatchTaskServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using ExampleOfHardToTestDesign.Services;
+using ExampleOfHardToTestDesign.Utilities;
+
+namespace ExampleOfHardToTestDesignTests.Services.Tests
+{
+    /// <summary>
+    /// 批次任務服務的測試類別。
+    /// </summary>
+    public class BatchTaskServiceTests
+    {
+        #region -- 前置準備 --
+
+        /// <summary>
+        /// 任務實用程式。
+        /// </summary>
+        private readonly ITaskHelper _taskHelper;
+
+        /// <summary>
+        /// JSON 實用程式。
+        /// </summary>
+        private readonly IJsonHelper _jsonHelper;
+
+        /// <summary>
+        /// 初始化 <see cref="BatchTaskServiceTests"/> 類別實體。
+        /// </summary>
+        public BatchTaskServiceTests()
+        {
+        }
+
+        private BatchTaskService GetSystemUnderTestInstance()
+        {
+            var sut = new BatchTaskService(this._taskHelper, this._jsonHelper);
+
+            return sut;
+        }
+
+        #endregion -- 前置準備 --
+    }
+}
