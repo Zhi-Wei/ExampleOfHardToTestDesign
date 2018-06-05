ee8c9e492d8fbd4726edb475dabf4ad3eb287f16
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-06-05 11:47:18 +0800

    新增 BatchTaskService.CreateBatchTasks 方法的測試案例。

--- ExampleOfHardToTestDesignTests/Services/BatchTaskServiceTests.cs
+++ ExampleOfHardToTestDesignTests/Services/BatchTaskServiceTests.cs
@@ -1,5 +1,13 @@
-﻿using ExampleOfHardToTestDesign.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExampleOfHardToTestDesign.Enums;
+using ExampleOfHardToTestDesign.Models;
+using ExampleOfHardToTestDesign.Services;
 using ExampleOfHardToTestDesign.Utilities;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
 
 namespace ExampleOfHardToTestDesignTests.Services.Tests
 {
@@ -25,8 +33,14 @@ namespace ExampleOfHardToTestDesignTests.Services.Tests
         /// </summary>
         public BatchTaskServiceTests()
         {
+            this._taskHelper = Substitute.For<ITaskHelper>();
+            this._jsonHelper = Substitute.For<IJsonHelper>();
         }
 
+        /// <summary>
+        /// 取得被測系統實體。
+        /// </summary>
+        /// <returns>被測系統實體。</returns>
         private BatchTaskService GetSystemUnderTestInstance()
         {
             var sut = new BatchTaskService(this._taskHelper, this._jsonHelper);
@@ -35,5 +49,74 @@ namespace ExampleOfHardToTestDesignTests.Services.Tests
         }
 
         #endregion -- 前置準備 --
+
+        #region -- CreateBatchTasks --
+
+        /// <summary>
+        /// Given   batchTaskData 為 Null
+        /// When    執行 CreateBatchTasks
+        /// Then    應拋出 ArgumentNullException 的例外狀況
+        /// </summary>
+        [Fact]
+        [Trait(nameof(BatchTaskService), "CreateBatchTasks")]
+        public void CreateBatchTasks_BatchTaskData_Is_Null_Should_Throw_ArgumentNullException()
+        {
+            // Arrange
+            IEnumerable<BatchTaskDataEntity> batchTaskData = null;
+
+            // Act
+            Action act = () => this.GetSystemUnderTestInstance().CreateBatchTasks(batchTaskData);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>()
+                .And.Message.Should().Be($"值不能為 null。{Environment.NewLine}參數名稱: batchTaskData");
+        }
+
+        /// <summary>
+        /// Given   batchTaskData 不為空
+        /// Given   DateTimeNow 為 9 時
+        /// When    執行 CreateBatchTasks
+        /// Then    應執行 _taskHelper.CreateTask 的方法並每個任務的預訂時間間隔一分鐘
+        /// </summary>
+        [Fact]
+        [Trait(nameof(BatchTaskService), "CreateBatchTasks")]
+        public void CreateBatchTasks_DateTimeNow_Nine_Clock_Each_Task_Should_Be_Intervals_One_Minutes()
+        {
+            // Arrange
+            int intervals = 1;
+            var dateTimeNow = new DateTime(2018, 6, 1, 9, 0, 0);
+            var batchTaskData = new List<BatchTaskDataEntity>
+            {
+                new BatchTaskDataEntity { Data = "A" },
+                new BatchTaskDataEntity { Data = "B" }
+            };
+
+            var batchTaskDataA = "{ \"Data\": \"A\" }";
+            var batchTaskDataB = "{ \"Data\": \"B\" }";
+
+            var bookingTimeA = dateTimeNow.AddMinutes(intervals);
+            var bookingTimeB = bookingTimeA.AddMinutes(intervals);
+
+            var sut = this.GetSystemUnderTestInstance();
+            sut.DateTimeNow = () => dateTimeNow;
+
+            this._jsonHelper.SerializeObject(Arg.Is(batchTaskData.First())).Returns(batchTaskDataA);
+            this._jsonHelper.SerializeObject(Arg.Is(batchTaskData.Last())).Returns(batchTaskDataB);
+
+            this._taskHelper.CreateTask(Arg.Any<TaskTypeEnum>(), Arg.Any<string>(), Arg.Any<DateTime?>())
+                .Returns(true);
+
+            // Act
+            var actual = sut.CreateBatchTasks(batchTaskData);
+
+            // Assert
+            this._taskHelper.Received(1)
+                .CreateTask(Arg.Is(TaskTypeEnum.BatchTask), Arg.Is(batchTaskDataA), Arg.Is(bookingTimeA));
+            this._taskHelper.Received(1)
+                .CreateTask(Arg.Is(TaskTypeEnum.BatchTask), Arg.Is(batchTaskDataB), Arg.Is(bookingTimeB));
+            actual.Should().BeTrue();
+        }
+
+        #endregion -- CreateBatchTasks --
     }
 }
